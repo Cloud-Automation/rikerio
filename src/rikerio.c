55e81f0b114ef75dd1dd7bc2a20cdfacb2270011
Stefan Pöter <stefan.poeter@cloud-automation.de>
2018-10-31 12:04:09 +0100

    Inspecting sget.

--- src/rikerio.c
+++ src/rikerio.c
@@ -861,9 +861,9 @@ int rio_sget(char* id, key_t* key) {
         return -1;
     }
 
-    int rret = fread(key, sizeof(*key), 1, fp);
+    int rret = fread(key, sizeof(key_t), 1, fp);
 
-    if (rret != sizeof(*key)) {
+    if (rret != sizeof(key_t)) {
         fclose(fp);
         return -1;
     }
