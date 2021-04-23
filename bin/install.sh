66a94dd68fa3a2fc6c602582af5245c465529dd4
Stefan Pöter <stefan.poeter@cloud-automation.de>
2021-04-23 15:51:52 +0200

    - Added some stuff to the postinst script.

--- bin/install.sh
+++ bin/install.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-groupadd rikerio
+GROUP=rikerio
+
+if ! [ $(getent group $GROUP) ]; then
+  groupadd $GROUP
+fi
+
+
+mkdir -p /etc/rikerio
+chown root:rikerio /etc/rikerio
+
+mkdir -p /var/lib/rikerio
+mkdir -p /var/run/rikerio
+
+chown root:rikerio /var/lib/rikerio
+chown root:rikerio /var/run/rikerio
+
 chown root:rikerio @CMAKE_INSTALL_PREFIX@/include/rikerio.h
 chown root:rikerio @CMAKE_INSTALL_PREFIX@/bin/{rio,rio-alias-rm,rio-alias-inspect,rio-alias-add}
 chmod 750 @CMAKE_INSTALL_PREFIX@/bin/{rio,rio-alias-rm,rio-alias-inspect,rio-alias-add}
